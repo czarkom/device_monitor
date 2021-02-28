392037e449353ad3c85c4e6acfd1f087b5d66d2d
Maciej Czarkowski <maciej.czarkowski@emergeconsulting.pl>
2021-02-28 12:41:46 +0100

    add logging add docs to package

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import time
-import test_device as td
+import maciej_device_manager as td
 import threading
 
 td.get_statuses('devices')
 print("Zaraz startuje")
 td.start('devices', 1)
-time.sleep(5)
+# for thread in threading.enumerate():
+#     print(thread.name)
+time.sleep(5.2)
 print("dupa")
 time.sleep(4)
 print("znow spalem")
 td.stop()
-#
+time.sleep(5)
 # for thread in threading.enumerate():
 #     print(thread.name)
 
-
-print("dupa")
 print("Exiting Main Thread")
